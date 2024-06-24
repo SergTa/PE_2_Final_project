96f43f96a2a4428e82f87e7360585b7b30a90a4e
SergTa <tabakar456@mail.ru>
2024-06-25 00:35:38 +0300

    Deleted main.py

--- main.py
+++
@@ -1,9 +0,0 @@
-from fastapi import FastAPI
-
-from summary_text.src.router import router as router_summary_text
-
-app = FastAPI(
-    title="Common api"
-)
-
-app.include_router(router_summary_text)
