6e11cc02542913a44004d7cd33db551a79140aa1
SergTa <88887008+SergTa@users.noreply.github.com>
2024-06-24 22:31:00 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 
-from src.router import router as router_summary_text
+from summary_text.src.router import router as router_summary_text
 
 app = FastAPI(
     title="Common api"
