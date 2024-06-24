e9b3a39b602e51cfae22962e29af56e9f340e547
SergTa <tabakar456@mail.ru>
2024-06-24 17:37:29 +0300

    Edited main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 
-from PW5.summary_text.src.router import router as router_summary_text
+from src.router import router as router_summary_text
 
 app = FastAPI(
     title="Common api"
