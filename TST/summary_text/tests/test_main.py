030dbf7314293d1cec8c9b574965acf061cd2add
SergTa <tabakar456@mail.ru>
2024-06-25 01:45:24 +0300

    edited test

--- TST/summary_text/tests/test_main.py
+++ TST/summary_text/tests/test_main.py
@@ -1,5 +1,9 @@
 from fastapi.testclient import TestClient
-from TST.main import app
+#from TST.main import app
+app = FastAPI(
+    title="Common api"
+)
+
 
 ENG_TXT = "This is a test message in English this test will test whether the application can generate a summary." \
           "If the  text is short, the application will return it as a summary"
