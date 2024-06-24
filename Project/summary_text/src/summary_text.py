c6a2076bb0bb11e3a813d460a16d64bfb0e0c192
SergTa <tabakar456@mail.ru>
2024-06-25 02:18:12 +0300

    edited summary_text.py

--- Project/summary_text/src/summary_text.py
+++ Project/summary_text/src/summary_text.py
@@ -6,7 +6,6 @@ import torch
 from transformers import (
     AutoTokenizer,
     AutoModelForSeq2SeqLM,
-    AutoModelForSpeechSeq2Seq,
     AutoProcessor,
     pipeline,
 )
@@ -31,32 +30,6 @@ def load_summary_model():
 
 
 @st.cache_resource
-def load_whisper_model():
-    # Загрузка модели whisper-large-v3
-    device = "cuda:0" if torch.cuda.is_available() else "cpu"
-    torch_dtype = torch.float16 if torch.cuda.is_available() else torch.float32
-    model_id = "openai/whisper-large-v3"
-    model = AutoModelForSpeechSeq2Seq.from_pretrained(
-        model_id,
-        torch_dtype=torch_dtype,
-        low_cpu_mem_usage=True,
-        use_safetensors=True,
-    )
-    model.to(device)
-    processor = AutoProcessor.from_pretrained(model_id)
-    return pipeline(
-        "automatic-speech-recognition",
-        model=model,
-        tokenizer=processor.tokenizer,
-        feature_extractor=processor.feature_extractor,
-        max_new_tokens=128,
-        chunk_length_s=30,
-        batch_size=16,
-        return_timestamps=True,
-        torch_dtype=torch_dtype,
-        device=device,
-    )
-
 def detect_encoding(data: bytes) -> str:
     # определение кодировки символов
     return detect(data)["encoding"]
@@ -64,8 +37,6 @@ def detect_encoding(data: bytes) -> str:
 def main():
     # загружаем предварительно обученную модель summary
     summary_text = load_summary_model()
-    # загружаем предварительно обученную модель wisper
-    whisper = load_whisper_model()
     # инициализируем переменные с текстом
     if "text" not in st.session_state:
         st.session_state["text"] = ""
